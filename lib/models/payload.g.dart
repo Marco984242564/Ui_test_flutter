5e9ad11b736d7247acff72b4350efe4865087d95
Marco Del Angel <marco-del-angel@hotmail.com>
2022-10-27 12:44:44 -0500

    commit7

---
+++ lib/models/payload.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'payload.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Payload _$PayloadFromJson(Map<String, dynamic> json) => Payload(
+      payload: json['payload'] as String,
+    );
+
+Map<String, dynamic> _$PayloadToJson(Payload instance) => <String, dynamic>{
+      'payload': instance.payload,
+    };
---
+++ lib/models/tecs.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tecs.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Tech _$TechFromJson(Map<String, dynamic> json) => Tech(
+      json['Techs'] as String,
+    );
+
+Map<String, dynamic> _$TechToJson(Tech instance) => <String, dynamic>{
+      'Techs': instance.Techs,
+    };
